05516c3fa49dad973999abfd47505cfd32a56330
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2025-04-28 13:12:10 -0500

    add things

--- installRpackages.py
+++ installRpackages.py
@@ -1,39 +1,73 @@
 import os
 
-fin = open("R/diff.txt","r")
+os.chdir("/scratch/g/rccadmin/mkeith/R")
+v_new = "4.5.0"
+v_old = "4.4.2"
+
+# Get the list of missing packages
+# Opening in write mode will empty it's previous content if file exist
+fnew = open(v_new+".txt",'w')
+for item in sorted(os.listdir("/hpc/apps/R/"+v_new+"/lib64/R/library/"), key=str.casefold):
+	if not item.startswith("00LOCK"):
+		_ = fnew.write(item+"\n")
+fnew.close()
+
+fold = open(v_old+".txt",'w')
+for item in sorted(os.listdir("/hpc/apps/R/"+v_old+"/lib64/R/library/"), key=str.casefold):
+        if not item.startswith("00LOCK"):
+                _ = fold.write(item+"\n")
+fold.close()
+
+if os.path.exists("diff.txt"):
+	 os.remove("diff.txt")
+os.system("diff "+v_new+".txt "+v_old+".txt >> diff.txt")
+
+# Create files to store the libraries that successfully & unsuccessfully installed
+fout1 = open("sucess.txt",'w')
+fout1.close()
+fout2 = open("fail.txt",'w')
+fout2.close()
+
+# Install missing packages
+fin = open("diff.txt","r")
 for line in fin:
 	line = line.replace("\n","").replace("> ","")
-	fout1 = open("R/sucess.txt","r")
-	fout2 = open("R/fail.txt","r")
-	output1 = os.popen("cat R/sucess.txt | grep "+line+" | head -n 1").read().replace("\n","")
-	output2 = os.popen("cat R/fail.txt | grep "+line+" | head -n 1").read().replace("\n","")
+	if line.startswith("<") or line[0].isdigit():
+		continue
+
+	fout1 = open("sucess.txt","r")
+	fout2 = open("fail.txt","r")
+	output1 = os.popen("cat sucess.txt | grep "+line+" | head -n 1").read().replace("\n","")
+	output2 = os.popen("cat fail.txt | grep "+line+" | head -n 1").read().replace("\n","")
 	fout2.close()
 	fout1.close()
 	if output1==line or output2==line:
 		continue
 
+	# Using the default install method
 	print("Installing "+line+"...\n")
 	print("Rscript -e \"install.packages('"+line+"')\"")
 	os.system("Rscript -e \"install.packages('"+line+"')\"")
 
 	if os.path.isdir("/hpc/apps/R/4.5.0/lib64/R/library/"+line+"/"):
 		print("\nsucess\n")
-		fout1 = open("R/sucess.txt","a")
-		fout1.write(line+"\n")
+		fout1 = open("sucess.txt","a")
+		_ = fout1.write(line+"\n")
 		fout1.close()
 	else:
+		# Using Bioconductor
 		print("\nTrying with bioconductor...")
 		print("Rscript -e \"BiocManager::install(c('"+line+"'))\"")
 		os.system("Rscript -e \"BiocManager::install(c('"+line+"'))\"")
 		
 		if os.path.isdir("/hpc/apps/R/4.5.0/lib64/R/library/"+line+"/"):
 			print("\nsucess\n")
-			fout1 = open("R/sucess.txt","a")
-			fout1.write(line+"\n")
+			fout1 = open("sucess.txt","a")
+			_ = fout1.write(line+"\n")
 			fout1.close()
 		else:
 			print("\nfailure\n")
-			fout2 = open("R/fail.txt","a")
-			fout2.write(line+"\n")
+			fout2 = open("fail.txt","a")
+			_ = fout2.write(line+"\n")
 			fout2.close()
 fin.close()
