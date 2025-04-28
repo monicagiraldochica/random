762ee36c71ba7cf4faa93bee46a7f53ef80feea8
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2025-04-28 09:36:48 -0500

    add option to install with bioconductor

--- installRpackages.py
+++ installRpackages.py
@@ -1,30 +1,39 @@
 import os
 
 fin = open("R/diff.txt","r")
-fout1 = open("R/sucess.txt","a+")
-fout2 = open("R/fail.txt","a+")
-
 for line in fin:
 	line = line.replace("\n","").replace("> ","")
+	fout1 = open("R/sucess.txt","r")
+	fout2 = open("R/fail.txt","r")
 	output1 = os.popen("cat R/sucess.txt | grep "+line+" | head -n 1").read().replace("\n","")
 	output2 = os.popen("cat R/fail.txt | grep "+line+" | head -n 1").read().replace("\n","")
+	fout2.close()
+	fout1.close()
 	if output1==line or output2==line:
 		continue
 
 	print("Installing "+line+"...\n")
 	print("Rscript -e \"install.packages('"+line+"')\"")
-	try:
-		os.system("Rscript -e \"install.packages('"+line+"')\"")
-	except:
-		print("exception")
+	os.system("Rscript -e \"install.packages('"+line+"')\"")
 
 	if os.path.isdir("/hpc/apps/R/4.5.0/lib64/R/library/"+line+"/"):
 		print("\nsucess\n")
+		fout1 = open("R/sucess.txt","a")
 		fout1.write(line+"\n")
+		fout1.close()
 	else:
-		print("\nfailure\n")
-		fout2.write(line+"\n")
+		print("\nTrying with bioconductor...")
+		print("Rscript -e \"BiocManager::install(c('"+line+"'))\"")
+		os.system("Rscript -e \"BiocManager::install(c('"+line+"'))\"")
 		
-fout2.close()
-fout1.close()
+		if os.path.isdir("/hpc/apps/R/4.5.0/lib64/R/library/"+line+"/"):
+			print("\nsucess\n")
+			fout1 = open("R/sucess.txt","a")
+			fout1.write(line+"\n")
+			fout1.close()
+		else:
+			print("\nfailure\n")
+			fout2 = open("R/fail.txt","a")
+			fout2.write(line+"\n")
+			fout2.close()
 fin.close()
