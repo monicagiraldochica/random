6011a4aa302f2821e76458bf1642c89d4028ff74
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2025-11-12 10:28:06 -0600

    Update biggest_subdirs.sh

--- biggest_subdirs.sh
+++ biggest_subdirs.sh
@@ -37,17 +37,8 @@ parse_args() {
 ## Main code
 parse_args "$@"
 
-#find "${searchdir}" -mindepth 1 -maxdepth 1 \( -name ".*" -o -name "*" \) -exec sh -c '
-#  for path do
-#	echo $path
-#    size=$(du -sh "$path" 2>/dev/null | awk "{print \$1}")
-#    owner=$(stat -c "%U" "$path")
-#    echo -e "$size\t$owner\t$path"
-#  done
-#' sh {} + | sort -h | tail -n "$nlines"
-
 for path in "$searchdir"/*; do
 	size=$(du -sh "$path" 2>/dev/null | awk "{print \$1}")
 	owner=$(stat -c "%U" "$path")
     echo -e "$size\t$owner\t$path"
-done | sort -hr
+done | sort -hr | tail -n "$nlines"
