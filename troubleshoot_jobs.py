d2108291195a17231af88b394e108baf6df82db7
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2025-11-19 13:13:32 -0600

    Update troubleshoot_jobs.py

--- troubleshoot_jobs.py
+++ troubleshoot_jobs.py
@@ -38,7 +38,7 @@ def get_jobInfo_scontrol(job_id):
     df = df[~df["Value"].isin([None, '', "(null)", "None"])]
     for col in ["ReqTRES", "AllocTRES"]:
         df.loc[df["Field"]==col, "Value"] = df.loc[df["Field"]==col, "Value"].str.replace(r',billing=.*$', '', regex=True)
-    df.loc[df["Field"]=="UserId", "Value"] = df.loc[df["Field"]=="UserId", "Value"].str.replace(r'(.*$', '', regex=True)
+    df.loc[df["Field"]=="UserId", "Value"] = df.loc[df["Field"]=="UserId", "Value"].str.replace(r'\(.*$', '', regex=True)
     df = df.reset_index(drop=True)
 
     return df
