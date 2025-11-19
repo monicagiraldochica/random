730580cf2ff6b132f689414ca9bc0e222c3eb582
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2025-11-19 10:26:09 -0600

    Update troubleshoot_jobs.py

--- troubleshoot_jobs.py
+++ troubleshoot_jobs.py
@@ -61,6 +61,8 @@ def get_jobInfo_sacct(job_id):
     #if len(output)==0:
     #    return pd.DataFrame()
     print(output)
+    output = output.splitlines()
+    print(output)
     
     #first_line = next((line for line in output if line.strip()), None)
     #print(first_line)
