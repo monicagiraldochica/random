e58d686c6f61bd38624e258cb02fda95cf2aca4a
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2025-11-20 11:31:00 -0600

    Update troubleshoot_jobs.py

--- troubleshoot_jobs.py
+++ troubleshoot_jobs.py
@@ -82,7 +82,7 @@ def get_jobInfo_sacct(job_id):
     
     # Merge Req resources lines into one
     new_vals = []
-    for i in range(3):
+    for i in range(len(titles)):
         cpus = df.query("Field=='ReqCPUS'")[titles[i]].iloc[0]
         mem = df.query("Field=='ReqMem'")[titles[i]].iloc[0]
         nodes = len(df.query("Field=='NodeList'")[titles[i]].iloc[0].split(","))
