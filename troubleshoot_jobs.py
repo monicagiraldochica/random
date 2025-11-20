79728da60fca96af3879f7bb2b8fddcc0599444e
monicagiraldochica <165931199+monicagiraldochica@users.noreply.github.com>
2025-11-20 10:50:14 -0600

    Update troubleshoot_jobs.py

--- troubleshoot_jobs.py
+++ troubleshoot_jobs.py
@@ -66,7 +66,7 @@ def get_jobInfo_sacct(job_id):
     if len(output)<3:
         return pd.DataFrame()
     
-    first_line = output[0]
+    first_line = output[0].replace("sys/dashb+", "sys/dashb+ (ondemand)")
     second_line = output[1]
     third_line = output[2]
     if len(first_line)<len(fields) or len(second_line)<len(fields) or len(third_line)<len(fields):
